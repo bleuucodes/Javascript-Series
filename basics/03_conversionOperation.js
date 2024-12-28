bac82ec82a898a1b8921a9c547f1cc5cff11260a
Anshuya Rawat <anshuyarawat8@gmail.com>
2024-12-28 17:10:42 +0000

    conversion of datatypes

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNum = Number(score)
+// console.log(typeof valueInNum)
+// console.log(valueInNum)
+
+//"33" => 33
+// "33abc" = NaN
+// true = 1
+// false = 0
+
+let isLoggedin = ""
+let booleanConversion = Boolean(isLoggedin)
+// console.log(booleanConversion)
+
+// 1 = true
+// 0 = false
+// "" = false
+// "hi" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
