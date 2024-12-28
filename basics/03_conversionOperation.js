8808a3df6efd84b249bad6b3cdc5968c585033fd
Anshuya Rawat <anshuyarawat8@gmail.com>
2024-12-28 17:38:17 +0000

    conversion

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -22,7 +22,45 @@ let booleanConversion = Boolean(isLoggedin)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ********************* Operations *************
+
+let value = 3
+let negValue = -value
 
 
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2%2)
+
+
+let str1= "hi"
+let str2= " ans"
+let str3= str1+ str2
+// console.log(str3)
+
+// console.log("1" + 2)  //12
+// console.log(1 + "2")  //12
+// console.log("1" + 2+ 2)  //122
+// console.log(1 + 2 + "2")  //32
+
+// console.log(+true) //  1
+
+//prefix and postfix values in mdn web docs
+
+// let counter= 10;
+// counter++
+// console.log(counter) //11
+
+
+
+
+
+
+
+ 
