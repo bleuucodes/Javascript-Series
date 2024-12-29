56fd8572e6c481093715aae085252e994cacb180
Anshuya Rawat <anshuyarawat8@gmail.com>
2024-12-29 13:23:08 +0000

    datatypes summary

---
+++ basics/datatypes.js
@@ -0,0 +1,12 @@
+const heros = []
+let myobj = {
+    name:"",
+    age: 22,
+}
+
+const myfunc = function(){
+    console.log("hi")
+}
+
+const bigNumber = 2132454656656n
+console.log(typeof bigNumber)
