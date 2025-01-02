9b24164ff46201d883602fcf9ea20e779d403743
Anshuya Rawat <anshuyarawat8@gmail.com>
2025-01-02 17:14:46 +0000

    string

---
+++ basics/05_strings.js
@@ -0,0 +1,38 @@
+ const name = "ansh"
+ const repo = 50
+//  console.log(`hi my name is ${name} and my repo count is ${repo}`)
+
+//  console.log(name[0])
+
+const game = new String("pokemon")
+
+// console.log(game)
+// console.log(game[2])
+// console.log(game.__proto__)
+
+console.log(game.length)
+console.log(game.toUpperCase()) // will not change original value
+console.log(game)
+console.log(game.charAt(2))
+console.log(game.indexOf('e'))
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+
+
