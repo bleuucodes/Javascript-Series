4bd6dc62f0500741258623f60cc8a80d73bb3dd6
Anshuya Rawat <anshuyarawat8@gmail.com>
2025-01-05 17:52:36 +0000

    this keyword

--- 03_basics/3_arrow.js
+++ 03_basics/3_arrow.js
@@ -3,19 +3,24 @@ const user = {
     name:"anshuya",
     greeting : function (){
         console.log(`Welcome ${this.name}`)    //o/p Welcome anshuya
+        console.log(this)                      //o/p  { name: 'anshuya', greeting: [Function: greeting] }
     }
 }
 // user.greeting()
 
-// console.log(this)      //o/p {}
+// console.log(this)              //o/p {}
+
+
 
 
 //////// this in function
 function hi(){
     const first ="Anshuya"
     console.log(this.first)      //o/p undefined
+    console.log(this)            // values
 }
 // hi()
+
 
 
 
@@ -24,6 +29,10 @@ function hi(){
 const arrowFunc = () => {
     let name = "anshuya"
     console.log(this.name)      //o/p undefined
+    console.log(this)           //o/p {}  //same as in node env global context
 }
 
-arrowFunc()
+// arrowFunc()
+
+
+
