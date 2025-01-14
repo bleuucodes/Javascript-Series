c2edc69798759f9211df15eca80bec9aad954e7d
Anshuya Rawat <anshuyarawat8@gmail.com>
2025-01-14 17:15:59 +0000

    getter setter through class

--- 10_classesAnd_oop/10_getSetinClass.js
+++ 10_classesAnd_oop/10_getSetinClass.js
@@ -22,3 +22,27 @@ class User {
 
 const hitesh = new User("h@hitesh.ai", "abc")
 console.log(hitesh.email);
+
+class Student{
+    constructor(rollNo,sName,sClass,contact){
+        this.rollNo = rollNo;
+        this.sName = sName;
+        this.sClass = sClass;
+        this.contact = contact;
+    }
+    get contact(){
+        return this._contact/2
+    }
+    set contact(contact){
+        this._contact = contact*2
+    }
+}
+
+const student1 = new Student(1,'anshuya',8,9528719000)
+// console.log(student1.sName)
+// console.log(student1.rollNo)
+// console.log(student1)
+// console.log(student1.contact)
+
+
+
