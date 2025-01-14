f0e275629b6f3b6a1fcb74d8bb3caa3b3019cc04
Anshuya Rawat <anshuyarawat8@gmail.com>
2025-01-14 17:23:21 +0000

    object properties

--- 10_classesAnd_oop/9_math.js
+++ 10_classesAnd_oop/9_math.js
@@ -2,9 +2,9 @@ const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
 
 // console.log(descripter);
 
-// console.log(Math.PI);
-// Math.PI = 5
-// console.log(Math.PI);
+console.log(Math.PI);
+Math.PI = 5
+console.log(Math.PI);
 
 const chai = {
     name: 'ginger chai',
